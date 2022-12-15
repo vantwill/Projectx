b409fd7c8958e9dc068c18aea92ecb3dd9a0a5c7
vantwill <vtwill41@gmail>
2022-12-15 12:23:29 +0300

    dev

---
+++ Frontend/src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            
+            <h5>Information</h5>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            
+            <h5>Service</h5>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            
+            <h5>Contact</h5>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxx</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
